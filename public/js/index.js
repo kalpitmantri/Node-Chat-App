088bea2d45c6ce01e3c3466f6a17f964512ef8c0
Kalpit <kalpitmantri98@gmail.com>
2018-12-21 19:17:27 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,12 @@ var socket = io();
 
 socket.on('connect',function(){
 	console.log('connected to server');
-	socket.emit('createMessage',{
-		from:'kalpit  ',
-		text:'Hey!! this is Kalpit!'
-	});
 });
 
 socket.on('disconnect',function(){
 	console.log('disconnected from server');
 });
 
-socket.on('newMessaage',function(message){
+socket.on('newMessage',function(message){
 	console.log('New message',message);
 });
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
 	console.log('New User Connected!!');
 
-	socket.emit('newMessage ',{
-		from:'Kalpit',
-		text:'Hey!! How are You??',
-		createAt:123
-	});  
-
 	socket.on('createMessage',(message)=>{
 		console.log('createMessage',message);
+		io.emit('newMessage',{
+			from:message.from,
+			text:message.text,
+			createdAt:new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect',()=>{
