0936dce0c1e4006991c5486e7bc5a73376991d80
Kalpit <kalpitmantri98@gmail.com>
2018-12-22 16:56:43 +0530

    Added form and showed incomig messages

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection',(socket)=>{
 
 	socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'))
 
-	socket.on('createMessage',(message)=>{
+	socket.on('createMessage',(message,callback)=>{
 		console.log('createMessage',message);
 		io.emit('newMessage',generateMessage(message.from,message.text));
 		// socket.broadcast.emit('newMessage',{
@@ -29,6 +29,7 @@ io.on('connection',(socket)=>{
 		// 	text:message.text,
 		// 	createdAt:new Date().getTime()
 		// });
+		callback();
 	});
 
 	socket.on('disconnect',()=>{
